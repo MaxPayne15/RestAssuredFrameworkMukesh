51a70f42df8b24cb01526a61c5ba960026b41c39
MaxPayne15 <ms.73889@gmail.com>
2024-10-09 16:43:24 +0530

    Adding Tests and Utility Classes

---
+++ src/test/java/booker/CreateBooking.java
@@ -0,0 +1,32 @@
+package booker;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.RestUtils;
+
+import java.util.HashMap;
+
+public class CreateBooking {
+
+    @Test
+    public void createBooking() {
+
+        String endPoint = "https://restful-booker.herokuapp.com/booking";
+        String payload = "{\n" +
+                "    \"firstname\": \"Jim\",\n" +
+                "    \"lastname\": \"Brown\",\n" +
+                "    \"totalprice\": 111,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "        \"checkin\": \"2018-01-01\",\n" +
+                "        \"checkout\": \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\": \"Breakfast\"\n" +
+                "}";
+        Response response = RestUtils.performPost(endPoint, payload, new HashMap<>());
+        Assert.assertEquals(response.statusCode(), 200);
+    }
+}
